ec11ee0786592448263180af7709c5c7f41100ad
Karoly Vilenczi <karoly.vilenczi@gmail.com>
2024-11-01 05:34:39 -0400

    added views

--- onlinecourse/apps.py
+++ onlinecourse/apps.py
@@ -1,5 +1,8 @@
 from django.apps import AppConfig
+from django.db.models import BigAutoField
 
 
 class OnlinecourseConfig(AppConfig):
     name = 'onlinecourse'
+
+    DEFAULT_AUTO_FIELD = BigAutoField
